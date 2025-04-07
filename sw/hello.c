924cd7df8b96b056c8e0bf44673283889bcaf862
Matthew Modi <22771068+matthew-modi@users.noreply.github.com>
2025-04-07 15:46:33 -0400

    Update hello.c

--- sw/hello.c
+++ sw/hello.c
@@ -46,20 +46,6 @@ int main()
   int i;
   static const char filename[] = "/dev/vga_ball";
 
-  static const vga_ball_color_t colors[] = {
-    { 0xff, 0x00, 0x00 }, /* Red */
-    { 0x00, 0xff, 0x00 }, /* Green */
-    { 0x00, 0x00, 0xff }, /* Blue */
-    { 0xff, 0xff, 0x00 }, /* Yellow */
-    { 0x00, 0xff, 0xff }, /* Cyan */
-    { 0xff, 0x00, 0xff }, /* Magenta */
-    { 0x80, 0x80, 0x80 }, /* Gray */
-    { 0x00, 0x00, 0x00 }, /* Black */
-    { 0xff, 0xff, 0xff }  /* White */
-  };
-
-# define COLORS 9
-
   printf("VGA ball Userspace program started\n");
 
   if ( (vga_ball_fd = open(filename, O_RDWR)) == -1) {
@@ -67,15 +53,36 @@ int main()
     return -1;
   }
 
-  printf("initial state: ");
-  print_background_color();
+  int r, g, b;
+  float h, s, v = 1.0;
 
-  for (i = 0 ; i < 24 ; i++) {
-    set_background_color(&colors[i % COLORS ]);
+  for (;;) {
+    hsv_to_rgb(h, s, v, &r, &g, &b);
+    h += 1.0; // Increment hue
+    if (h >= 360.0) h = 0.0; // Wrap around hue
+    set_background_color({r, g, b});
     print_background_color();
     usleep(400000);
   }
   
   printf("VGA BALL Userspace program terminating\n");
   return 0;
+}
+
+void hsv_to_rgb(float h, float s, float v, int *r, int *g, int *b) {
+    float c = v * s;
+    float x = c * (1 - fabs(fmod(h / 60.0, 2) - 1));
+    float m = v - c;
+    float r1, g1, b1;
+
+    if (h < 60)       { r1 = c; g1 = x; b1 = 0; }
+    else if (h < 120) { r1 = x; g1 = c; b1 = 0; }
+    else if (h < 180) { r1 = 0; g1 = c; b1 = x; }
+    else if (h < 240) { r1 = 0; g1 = x; b1 = c; }
+    else if (h < 300) { r1 = x; g1 = 0; b1 = c; }
+    else              { r1 = c; g1 = 0; b1 = x; }
+
+    *r = (int)((r1 + m) * 255);
+    *g = (int)((g1 + m) * 255);
+    *b = (int)((b1 + m) * 255);
 }
