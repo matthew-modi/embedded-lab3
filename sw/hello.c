33e1e3d3b1fe649bf814dca82b8ff5f3269230a6
Matthew Modi <22771068+matthew-modi@users.noreply.github.com>
2025-04-09 14:53:47 -0400

    Update hello.c

--- sw/hello.c
+++ sw/hello.c
@@ -174,7 +174,7 @@ int main()
         y = 0;
         
         vga_ball_position_t position = { // map x and y (0 to 1) to ints from 0 to 65535
-            (unsigned short)(0), 
+            (unsigned short)(50), 
             (unsigned short)(50)};
         set_position(&position);
         print_position();
